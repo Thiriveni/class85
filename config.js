2f09ec7fcaa6b38a1292c44aa651daa07c3d1f25
Thiriveni <67469883+Thiriveni@users.noreply.github.com>
2022-04-05 19:59:08 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCW9PP68SsnRCXEr5dMXbRGHjSWoJZ9_yE",
+    authDomain: "storytellingapp-51bf6.firebaseapp.com",
+    projectId: "storytellingapp-51bf6",
+    storageBucket: "storytellingapp-51bf6.appspot.com",
+    messagingSenderId: "70242626086",
+    appId: "1:70242626086:web:d6b032bf2a5716964854e7"
+  };
+  
